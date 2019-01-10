d4beaa1cd4b4e8688b6da7b8e4c9d97c004af422
fat <fatgrammer.com>
2019-01-10 14:35:43 +0800

    change something

---
+++ src/main/kotlin/com/example/testdemo/TestController.kt
@@ -0,0 +1,39 @@
+package com.example.testdemo
+
+import com.demo.jwt.config.security.JwtAuth
+import com.demo.jwt.config.security.JwtProperties
+import com.demo.jwt.config.security.UriHandler
+import com.demo.jwt.service.impl.UserDetailsServiceAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+
+    @JwtAuth
+    @GetMapping("/")
+    fun some(): Any {
+        return JwtProperties
+    }
+
+    @GetMapping("/else")
+    fun index(): Any {
+//        var h : UriHandler
+        return UriHandler.uris
+
+    }
+
+    @Autowired
+    lateinit var userDetailsService: UserDetailsServiceAdapter
+
+    @PostMapping("/sign/up")
+    fun signUp(@RequestBody user: Map<String, String>): Any {
+        userDetailsService.saveUser(user["username"]!!, user["password"]!!)
+        return 0
+    }
+}
--- src/main/kotlin/com/example/testdemo/TestDemoApplication.kt
+++ src/main/kotlin/com/example/testdemo/TestDemoApplication.kt
@@ -1,29 +1,49 @@
 package com.example.testdemo
 
+import com.demo.jwt.service.impl.UserDetailsServiceAdapter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-@ComponentScan(
-        "com.demo.jwt"
-)
+import org.springframework.context.annotation.Primary
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+
 @SpringBootApplication
+(
+        scanBasePackages = [
+            "com.demo.jwt"
+            , "com.example"
+        ]
+)
 class TestDemoApplication
 
 fun main(args: Array<String>) {
     runApplication<TestDemoApplication>(*args)
 }
 
-@RestController
-class MainController {
-    @GetMapping("/")
-    fun index(): Any {
-
-//        var h : UriHandler
-        return ""
-
+@Service
+@Primary
+class UserDetailsServiceImpl : UserDetailsServiceAdapter() {
+    companion object {
+        val userData = mutableMapOf(
+                "fat" to BCryptPasswordEncoder().encode("pss")
+        )
     }
 
+    override fun loadUserByUsername(username: String): UserDetails {
+        val password = userData[username]
+                ?: throw UsernameNotFoundException(username)
+        return User(
+                username,
+                password,
+                emptyList()
+        )
+    }
+
+    override fun saveUser(username: String, password: String) {
+
+    }
 }
 
